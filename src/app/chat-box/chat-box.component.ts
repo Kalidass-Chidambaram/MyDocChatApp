711c85a30faac1907cc93135f7036a7ee85b9eda
kishanr946 <kishanr946@gmail.com>
2020-06-23 13:00:04 +0530

    server change

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -9,11 +9,32 @@ import { ChatService } from './service/chat.service';
 })
 export class ChatBoxComponent implements OnInit {
 
+  public messageToSend: string = '';
+  public userId: number;
+  public connectionType = 'chat';
+  public msgArray: any[] = [];
 
 
-  constructor(private chatServie: ChatService) { }
+  constructor(private chatServie: ChatService) {
+    if (localStorage.getItem("userId") === null) {
+      localStorage.setItem('userId', '1');
+      this.userId = 1;    
+    } else {
+      this.userId = 2;    
+    }
+    this.chatServie.newMessageReceived().subscribe((data)=> {
+      this.msgArray.push(data);
+      let msgContentDiv = document.getElementById('msg-content-area');
+      console.log(msgContentDiv.scrollHeight);
+      console.log(msgContentDiv.clientHeight);
+      if (msgContentDiv.scrollHeight > msgContentDiv.clientHeight) {
+        msgContentDiv.scrollTop = msgContentDiv.scrollHeight - msgContentDiv.clientHeight;
+      }
+    })
+   }
 
   ngOnInit(): void {
+    this.chatServie.joinChat({connectionType: this.connectionType, userId: this.userId});
     let msgContentDiv = document.getElementById('msg-content-area');
     console.log(msgContentDiv.scrollHeight);
     console.log(msgContentDiv.clientHeight);
@@ -22,4 +43,8 @@ export class ChatBoxComponent implements OnInit {
     }
   }
 
+  public sendMessage() {
+    this.chatServie.sendMessage({userId: this.userId, connectionType: this.connectionType, message: this.messageToSend});
+    this.messageToSend = '';
+  }
 }
--- src/app/chat-box/service/chat.service.ts
+++ src/app/chat-box/service/chat.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client'
+import { Observable } from 'rxjs';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,26 @@ export class ChatService {
 
   constructor() { }
 
-  // private _socket = io('http://localhost:3000');
+   private _socket = io('http://localhost:3000');
+
+   public joinChat(data) {
+     this._socket.emit('join', data);
+   }
+
+   public sendMessage(data) {
+    this._socket.emit('message', data);
+  }
+
+  public newMessageReceived() {
+    let observable = new Observable<{user: string, message:string}>(observer => {
+      this._socket.on('new message', (data)=>{
+        observer.next(data);
+      })
+      return () => { this._socket.disconnect();}
+    })
+
+    return observable;
+
+  }
 
 }
